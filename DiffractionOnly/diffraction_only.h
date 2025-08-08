b5e6d7f52e927f6e38cff8fd8d622e03892b1746
RaulEst50 <raulest50@gmail.com>
2025-08-08 00:10:09 +0000

    Remove compiled test binary

---
+++ DiffractionOnly/diffraction_only.h
@@ -0,0 +1,17 @@
+#ifndef DIFFRACTION_ONLY_H
+#define DIFFRACTION_ONLY_H
+
+#if __has_include(<hls_x_complex.h>)
+#  include <hls_x_complex.h>
+#else
+#  include "hls_stub.h"
+#endif
+
+const int N = 64;
+using data_t = float;
+using complex_t = hls::x_complex<data_t>;
+
+void diffraction_only(complex_t phi_in[N][N],
+                      complex_t phi_out[N][N]);
+
+#endif // DIFFRACTION_ONLY_H
